481d2b0d13816d3b3f3df541de9aad9f3801437c
Peter Hayes <peter.k.hayes@gmail.com>
2019-10-16 10:10:09 -0700

    Add gh-pages

--- src/redux/sounds.js
+++ src/redux/sounds.js
@@ -1,6 +1,6 @@
 // @flow
 import type { ReduxMiddleware } from './store';
-import type { Fighter } from '../types/Fighter';
+// import type { Fighter } from '../types/Fighter';
 import sample from 'lodash/sample';
 import {
   eitherPlayerVibesWentUp,
@@ -8,12 +8,21 @@ import {
   gameEndedSad,
   gameEndedHappy,
 } from '../utils/fighting.js';
+
+// TODO: figure this shit out
+// $FlowFixMe
 import soundHurray1 from '../sounds/hurray_1.mp3';
+// $FlowFixMe
 import soundSad1 from '../sounds/im_sad_1.mp3';
+// $FlowFixMe
 import soundNice1 from '../sounds/nice_1.mp3';
+// $FlowFixMe
 import soundThisSucks1 from '../sounds/this_sucks_1.mp3';
+// $FlowFixMe
 import soundWow1 from '../sounds/wow_1.mp3';
+// $FlowFixMe
 import soundBestFriends from '../sounds/best_friends.mp3';
+// $FlowFixMe
 import soundLeavingParty from '../sounds/leaving_party.mp3';
 
 type Sound = string;
@@ -23,7 +32,7 @@ const HURT_VIBES_SOUNDS = [soundSad1, soundThisSucks1];
 
 const soundsMiddleware: ReduxMiddleware = (store) => (next) => (action) => {
   const previousState = store.getState();
-  next(action);
+  const result = next(action);
   const currentState = store.getState();
 
   if (previousState.stage === 'fight' && currentState.stage === 'fight') {
@@ -37,6 +46,9 @@ const soundsMiddleware: ReduxMiddleware = (store) => (next) => (action) => {
       playSoundFromList(HURT_VIBES_SOUNDS);
     }
   }
+
+  // $FlowFixMe - hmmm
+  return result;
 };
 
 function playSoundFromList(list: Array<Sound>) {
